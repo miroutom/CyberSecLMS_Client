4da0613351a6cca58e753873848b7c1a7c583d9f
Erika Shimin <mirou.tomson@mail.ru>
2025-04-07 12:35:24 +0300

    remove all unnecessary code

--- platform_backend_python/app/schemas/schemas.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-from pydantic import BaseModel
-
-
-class BaseSchema(BaseModel):
-    created_at: datetime | None = None
-    updated_at: datetime | None = None
-
--- platform_backend_python/app/utils/exceptions.py
+++
@@ -1,7 +0,0 @@
-from fastapi import HTTPException
-from starlette import status
-
-unauthorized_exception = HTTPException(
-    detail=f'Invalid username or password or security code',
-    status_code=status.HTTP_401_UNAUTHORIZED,
-)
