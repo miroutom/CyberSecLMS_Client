b0db46d62e238db23745f1d6440ef3825798f72a
MelnikMarinaV <mvmelnik@edu.hse.ru>
2025-04-12 16:56:54 +0300

    Add styles to main page

--- cybersecurity_frontend/src/components/AllTasks.vue
+++ cybersecurity_frontend/src/components/AllTasks.vue
@@ -4,18 +4,48 @@
     <div class="content-wrapper">
       <TheSideBar />
       <main class="all-tasks">
-        <router-link
+        <div
+          class="card-container"
           v-for="vulnerability in vulnerabilities"
           :key="vulnerability.name"
-          :to="{
-            name: 'VulnerabilityTasks',
-            params: { vulnerability: vulnerability.name },
-          }"
-          class="vulnerability-card"
-          :style="{ backgroundColor: vulnerability.color }"
         >
-          {{ vulnerability.name }}
-        </router-link>
+          <router-link
+            :to="{
+              name: 'VulnerabilityTasks',
+              params: { vulnerability: vulnerability.name },
+            }"
+            class="card-link"
+          >
+            <div class="vulnerability-card">
+              <div
+                class="card-top"
+                :style="{
+                  background: vulnerability.gradient,
+                }"
+              >
+                <h2
+                  class="card-title"
+                  :style="{ backgroundImage: vulnerability.titleColor }"
+                >
+                  {{ vulnerability.name }}
+                </h2>
+              </div>
+              <div class="card-content">
+                <p class="card-description">{{ vulnerability.description }}</p>
+                <div
+                  class="task-count"
+                  :style="{ color: vulnerability.accentColor }"
+                >
+                  <span>{{ vulnerability.taskCount }} заданий</span>
+                  <hr
+                    class="line"
+                    :style="{ borderColor: vulnerability.accentColor }"
+                  />
+                </div>
+              </div>
+            </div>
+          </router-link>
+        </div>
       </main>
     </div>
   </div>
@@ -34,10 +64,35 @@ export default {
   data() {
     return {
       vulnerabilities: [
-        { name: "XSS", color: "#8ab5ff" },
-        { name: "CSRF", color: "#a0efde" },
-        { name: "SQL Injection", color: "#ffbe8a" },
-        { name: "XSS", color: "#8ab5ff" },
+        {
+          name: "XSS",
+          gradient: "linear-gradient(180deg, #F2F2FF 0%, #3764ED 100%)",
+          titleColor:
+            "linear-gradient(90deg, #3764ED 0%, #2C4FBC 50%, #1F3987 100%)",
+          description:
+            "XSS уязвимости - внедрение вредоносных скриптов на веб-страницы.",
+          taskCount: 10,
+          accentColor: "#3764ED",
+        },
+        {
+          name: "CSRF",
+          gradient: "linear-gradient(180deg, #F2F2FF 0%, #16B593 100%)",
+          titleColor:
+            "linear-gradient(90deg, #04916E 0%, #046E4C 50%, #004A2E 100%)",
+          description: "CSRF уязвимости - подделка межсайтовых запросов.",
+          taskCount: 15,
+          accentColor: "#3AE8C5",
+        },
+        {
+          name: "SQL Injection",
+          gradient: "linear-gradient(180deg, #FEF3E9 0%, #F9A866 100%)",
+          titleColor:
+            "linear-gradient(90deg, #F88F3A 0%, #DE7620 50%, #C6691D 100%)",
+          description:
+            "SQL Injection - внедрение вредоносного кода для манипуляции базой данных.",
+          taskCount: 12,
+          accentColor: "#F9A866",
+        },
         // ... other vulnerabilities
       ],
     };
@@ -50,8 +105,8 @@ export default {
   display: flex;
   flex-direction: column;
   min-height: 100vh;
+  font-family: "Arial", sans-serif;
 }
-
 .content-wrapper {
   display: flex;
   flex-grow: 1;
@@ -60,28 +115,84 @@ export default {
 .all-tasks {
   flex-grow: 1;
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
   gap: 20px;
   padding: 20px;
   margin-top: 10px;
-  grid-auto-rows: 400px;
+}
+
+.card-link {
+  text-decoration: none;
+}
+
+.card-container {
+  display: flex;
+  flex-direction: column;
 }
 
 .vulnerability-card {
+  width: 300px;
+  height: 400px;
+  background: #f2f2ff;
+  border-radius: 30px;
+  overflow: hidden;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+  transition: transform 0.3s ease;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 20px;
-  border-radius: 15px;
-  text-decoration: none;
-  color: #333;
-  transition: background-color 0.3s ease;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-  font-weight: bold;
-  font-size: 1.2em;
+  flex-direction: column;
+  font-family: "Montserrat", sans-serif;
 }
 
 .vulnerability-card:hover {
-  transform: translateY(-5px);
+  transform: scale(1.05);
+  box-shadow: 0 8px 20px rgba(0, 0, 0, 0.2); /* Увеличиваем размытие и смещение тени */
+}
+
+.card-top {
+  min-height: 200px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.card-content {
+  padding: 20px;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+}
+
+.card-title {
+  font-weight: 800;
+  font-size: 40px;
+  line-height: 49px;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  padding: 20px;
+  text-align: center;
+}
+
+.card-description {
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 20px;
+  color: #000000;
+  margin-bottom: 20px;
+}
+.task-count {
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 17px;
+  text-align: center;
+  margin-bottom: 10px;
+}
+.line {
+  width: 85px;
+  height: 0px;
+  border: 1px solid;
+  margin: 5px auto 0;
 }
 </style>
