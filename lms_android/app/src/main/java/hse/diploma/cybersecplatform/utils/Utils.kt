655b9c19427e90d5352be350d849e64c2155aa83
Erika Shimin <mirou.tomson@mail.ru>
2025-06-03 11:14:03 +0300

    add suspend utils

--- lms_android/app/src/main/java/hse/diploma/cybersecplatform/utils/Utils.kt
+++ lms_android/app/src/main/java/hse/diploma/cybersecplatform/utils/Utils.kt
@@ -60,30 +60,3 @@ fun maskEmail(email: String): String {
         namePart.first() + "*".repeat(nb - 2) + namePart.last() + "@" + domainPart
     }
 }
-
-/**
- * The extension that performs the operation with repeated attempts
- * @param maxRetries maximum number of attempts
- * @param initialDelayMs initial delay between attempts in milliseconds
- * @param maxDelayMs maximum delay between attempts
- * @param factor multiplier to increase the waiting time between attempts
- * @param block operation to be performed
- */
-suspend fun <T> retry(
-    maxRetries: Int = 3,
-    initialDelayMs: Long = 200,
-    maxDelayMs: Long = 5000,
-    factor: Double = 2.0,
-    block: suspend () -> Result<T>,
-): Result<T> {
-    var currentDelay = initialDelayMs
-    repeat(maxRetries - 1) {
-        val result = block()
-        if (result.isSuccess) return result
-
-        currentDelay = (currentDelay * factor).toLong().coerceAtMost(maxDelayMs)
-        delay(currentDelay)
-    }
-
-    return block()
-}
